b4337f47da8c411bd477d21ce1003b3dfb10fa06
AliiaaT <aliiatemirbekkkyzy@gmail.com>
2023-01-06 01:16:07 +0100

    added validations, but need to add email, date, price validations later!!

---
+++ MP1/js/validationInstructor.js
@@ -0,0 +1,86 @@
+
+function validateForm(){
+
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const priceInput = document.getElementById('price');
+    const licenseInput = document.getElementById('lecense');
+
+    const errorName = document.getElementById('errorName');
+    const errorEmail = document.getElementById('errorEmail');
+    const errorPrice = document.getElementById('errorPrice');
+    const errorLicense = document.getElementById('errorLicense');
+    const errorSummary = document.getElementById('errorSummary');
+
+    console.log("Before reset Errors");
+
+    resetErrors([nameInput, emailInput, priceInput, licenseInput], 
+                [errorName, errorEmail, errorPrice, errorLicense]
+    );
+
+    console.log("AFter reset Errors");
+    let valid = true;
+
+
+   //validation for name field
+   if (!checkRequired(nameInput.value)) {
+    valid = false;
+    nameInput.classList.add("error-input");
+    errorName.innerText = "The field is required.";
+}else if(!checkTextLengthRange(nameInput.value, 2, 60)) {
+    valid = false;
+    nameInput.classList.add("error-input");
+    errorName.innerText = "The field should contain 2 to 60 characters.";
+}
+
+
+
+//validation for email field
+if (!checkRequired(emailInput.value)) {
+    //ad email regex validation!!!
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "The field is required.";
+}
+
+
+
+//validation for price field
+if (!checkRequired(priceInput.value)) {
+    //add Price  validation!!!
+    valid = false;
+    priceInput.classList.add("error-input");
+    errorPrice.innerText = "The field is required.";
+}
+
+
+//validation for license field
+if (!checkRequired(licenseInput.value)) {
+    //ad Date validation!!! Like not grated then present timedate
+    valid = false;
+    licenseInput.classList.add("error-input");
+    errorLicense.innerText = "The field is required.";
+}
+
+
+
+if (valid) {
+    errorSummary.style.visibility = "hidden";
+    const conf = confirm('Do you want to add the record?');
+    return conf
+} else {
+    errorSummary.style.visibility = "visible";
+    return false;
+}
+
+};
+
+
+
+
+
+
+
+
+
+
---
+++ MP1/js/validationLesson.js
@@ -0,0 +1,80 @@
+
+function validateForm(){
+
+    const studentInput = document.getElementById('student');
+    const instructorInput = document.getElementById('instructor');
+    const startDateInput = document.getElementById('startDate');
+    const endDateInput = document.getElementById('endDate');
+
+    const errorStudent = document.getElementById('errorSName');
+    const errorInstructor = document.getElementById('errorIName');
+    const errorSDate = document.getElementById('errorSNumber');
+    const errorEDate = document.getElementById('errorENumber');
+    const errorSummary = document.getElementById('errorSummary');
+
+    console.log("Before reset Errors");
+
+    resetErrors([studentInput, instructorInput, startDateInput, endDateInput], 
+                [errorStudent, errorInstructor, errorSDate, errorEDate]
+    );
+
+
+    console.log("AFter reset Errors");
+    let valid = true;
+
+
+    //validation for Student field
+    if (!checkDropboxSelected(studentInput)) {
+        valid = false;
+        studentInput.classList.add("error-input");
+        errorStudent.innerText = "The field is required.";
+    }
+
+    //validation for Student field
+    if (!checkDropboxSelected(instructorInput)) {
+        valid = false;
+        instructorInput.classList.add("error-input");
+        errorInstructor.innerText = "The field is required.";
+    }
+
+
+
+    //validation for startDate field
+    if (!checkRequired(startDateInput.value)) {
+        //ad Date validation!!! Like not grated then present timedate
+        valid = false;
+        startDateInput.classList.add("error-input");
+        errorSDate.innerText = "The field is required.";
+    }
+
+
+    //validation for endDate field
+    if (!checkRequired(endDateInput.value)) {
+        //ad Date validation!!! Like not grated then present timedate
+        valid = false;
+        endDateInput.classList.add("error-input");
+        errorEDate.innerText = "The field is required.";
+    }
+
+
+
+    if (valid) {
+        errorSummary.style.visibility = "hidden";
+        const conf = confirm('Do you want to add the record?');
+        return conf
+    } else {
+        errorSummary.style.visibility = "visible";
+        return false;
+    }
+    
+
+};
+
+
+
+
+
+
+
+
+
